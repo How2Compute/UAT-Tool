9a8d7af270762156b90a6f66d7b3b43f718db7a1
HowToCompute <howtocompute123@gmail.com>
2020-01-04 17:07:25 +0100

    Implement parsing of LauncherInstalled.dat + a data structure to hold an engine install

--- main.cpp
+++ main.cpp
@@ -11,6 +11,13 @@
 // Abbreviate nlohmann::json to json
 using json = nlohmann::json;
 
+// Simple data structure to hold the name/path of an engine; will become more important when we allow the user to add custom paths (to source builds).
+struct EngineInstall
+{
+	std::string name;
+	std::string path;
+};
+
 int main(int argc, char *argv[])
 {
 	// We need at least 2 arguments (command name & the version to use)
@@ -56,7 +63,42 @@ int main(int argc, char *argv[])
 		return -3;
 	}
 
-	std::cout <<  launcherInstalledJson.dump();
+	// NOTE: Though this doesn't really have any use at this point in time (an if would work just fine too), we will likely use this in the future when we allow the user to save custom names/paths.
+	std::vector<EngineInstall> engineInstalls;
+
+	json installationListJson = launcherInstalledJson["InstallationList"];
+	for (json::iterator it = installationListJson.begin(); it != installationListJson.end(); ++it) {
+		// Attempt to parse this item, and if there was an error, skip the item.
+		try {
+			std::string appName = (*it)["AppName"].get<std::string>();
+			std::string appPath = (*it)["InstallLocation"].get<std::string>();
+
+			std::string appNamePrefix = "UE_";
+
+			// Ensure the prefix is in the name (this is the way we identify (luancher) builds over plugins & the like)
+			size_t prefix_position = appName.find(appNamePrefix);
+			if (prefix_position == std::string::npos)
+			{
+				continue;
+			}
+
+			// Erase the prefix from the string so we can compare it.
+			appName.erase(prefix_position, appNamePrefix.length());
+
+			// Add this entry to the list of installs.
+			EngineInstall install;
+			install.name = appName;
+			install.path = appPath;
+			engineInstalls.push_back(install);
+
+		}
+		catch (...)
+		{
+			continue;
+		}
+	}
+
+	std::cout << engineInstalls.size() << " Unreal Engine installs found!" << std::endl;
 
 	return 10;
 }
