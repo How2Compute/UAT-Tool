cc6546456ce461e32b4dc51f3dc64d2a5516b2e7
HowToCompute <howtocompute123@gmail.com>
2024-09-14 15:33:32 +0200

    Implement UAT command running, passing in multiple engines

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::process::Command;
+
 use clap::Parser;
 use colored::Colorize;
 use engine_utils::EngineInstall;
@@ -9,7 +11,7 @@ struct Args {
     #[clap(long = "list", short = 'o')]
     should_list: bool,
 
-    /// Version of Unreal Engine to use
+    /// Version(s) of Unreal Engine to use - "+" as separator
     engine_version: String,
 
     /// The UAT command to run
@@ -64,7 +66,7 @@ fn find_engine_version(installs: &Vec<EngineInstall>, name: &str) -> Option<Engi
         let major_version: Option<u8> = version_name.split('.').nth(0).or(Some("")).unwrap().parse().ok();
         let minor_version: Option<u8> = version_name.split('.').nth(1).or(Some("")).unwrap().parse().ok();
         let patch_version: Option<u8> = version_name.split('.').nth(2).or(Some("")).unwrap().parse().ok();
-        
+
         if (patch_version.is_some() && minor_version.is_none() || major_version.is_none()) {
             println!("{}: {}", "INVALID ENGINE VERSION".bold().red(), name);
             return None;
@@ -91,8 +93,8 @@ fn main() {
     // Parse the CLI and pull the versions of UE that are currently installed
     let args = Args::parse();
     let engine_installs = engine_utils::get_engine_installs().expect("Unable to fetch engine installs: ");
-    println!("Matched Engine Version: {:?}", find_engine_version(&engine_installs, &args.engine_version));
 
+    // Were we run with --list?
     if args.should_list {
         // List subcommand used - only list the installed engine versions
         println!("{}", "Unreal Engine Installs".bold().underline());
@@ -109,6 +111,61 @@ fn main() {
         }
         return;
     }
-    
-    
+    // Regular run
+    else {
+        // Split & look up the passed in Unreal Engine versions
+        let requested_engine_names: Vec<&str> = args.engine_version.split("+").collect();
+        let engines_to_use: Vec<(&str, Option<EngineInstall>)> = requested_engine_names.iter().map(|&version_name| (version_name, find_engine_version(&engine_installs, version_name))).collect();
+        let engines_not_found: Vec<&(&str, Option<EngineInstall>)> = engines_to_use.iter().filter(|(_, engine)| engine.is_none()).collect();
+
+        // Ensure all version names were matched to an engine on disk
+        if !engines_not_found.is_empty() {
+            // At least one engine version failed lookup!
+            println!("{}:", "Unable to resolve the following engine version(s)".bold().red());
+            for (name, _) in engines_not_found {
+                println!("- {}", name);
+            };
+
+            return;
+        }
+        
+        // Log the versions found to the user
+        println!("{}:", "Found engine version(s) at".bold().green());
+        for (name, engine) in &engines_to_use {
+            println!("- {} -> {}", name, engine.clone().unwrap().base_dir.to_str().unwrap_or("PATH ERROR"));
+        }
+        if engines_to_use.len() > 1 {
+            println!("{} {}", engines_to_use.len().to_string().bold().yellow(), "versions passed in!".bold().green());
+            println!("{}", "We will run your command once for each version, in the order listed above".italic());
+        }
+
+        for (name, engine) in engines_to_use {
+            println!("{} {}", "Running for version: ".bold(), name.yellow().bold());
+            static UAT_RELATIVE_PATH: &str = "Engine/Build/BatchFiles/RunUAT.bat";
+            let uat_path = engine.unwrap().base_dir.join(UAT_RELATIVE_PATH);
+            
+            // Log the command we are going to run
+            // TODO: Fix this weird iter clone thingy! There's got to be a better way!
+            let user_command = args.command.iter().map(|a| a.to_string()).collect::<Vec<_>>().join(" ");
+            let os_command = format!("{:?} {}", uat_path, user_command);
+            println!("Running: {}", os_command.italic());
+
+            // Start UAT with the command, wait for it to finish and log the code
+            let mut uat_invocation = Command::new(uat_path.as_os_str())
+            .args(args.command.iter())
+            .spawn()
+            .expect("UAT command failed to start!");
+            let exit_code = uat_invocation.wait().expect("Unable to wait for UAT completion").code().expect("Unable to retrieve output code");
+            println!("{}: {}", "UAT process exited with code".bold(), exit_code.to_string().yellow().bold());
+
+            // Fail the entire process if one exit code is non-zero/a failure
+            // TODO: Turn this into something optional?
+            if (exit_code != 0) {
+                println!("{}", "Non-zero exit code! Assuming process has failed, stopping further execution!".red().bold());
+                return;
+            }
+        }
+
+        println!("{}", "DONE!".bold().green())
+    }
 }
